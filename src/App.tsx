8daf4cb3c95ca815c793ecb7ce069ac18970b9b5
billy-yuan <billy.yuan1@gmail.com>
2021-11-25 10:53:09 -0500

    add graphql endpoint for blog posts

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import "./App.css";
+import { Blog } from "./blog/Blog";
 
 function App() {
-  return <div className="App"></div>;
+  const client = new ApolloClient({
+    uri: "https://api-us-east-1.graphcms.com/v2/ckwcbs3zf4d0k01xp350q8mde/master",
+    cache: new InMemoryCache(),
+  });
+
+  return (
+    <div className="App">
+      <ApolloProvider client={client}>
+        <Blog />
+      </ApolloProvider>
+    </div>
+  );
 }
 
 export default App;
