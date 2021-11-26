d2b34aff6b07eda0047dfdb8a71520a889301991
billy-yuan <billy.yuan1@gmail.com>
2021-11-26 17:24:17 -0500

    add nav menu when user clicks on top right

--- src/main-nav/MainNavIcons.tsx
+++ src/main-nav/MainNavIcons.tsx
@@ -1,13 +1,24 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import { NavMenu } from "./NavMenu";
 import "./style.css";
 
 export function MainNavIcons({ children }: any) {
+  const [isNavMenuVisible, toggleNavMenu] = useState<boolean>(false);
+
   return (
     <>
       <Link to="/">
         <div className="home-icon">BY</div>
       </Link>
-      <div className="nav-icon">=</div>
+      <div
+        className="nav-icon"
+        style={{ color: isNavMenuVisible ? "white" : "black" }}
+        onClick={() => toggleNavMenu(!isNavMenuVisible)}
+      >
+        {isNavMenuVisible ? "x" : "="}
+      </div>
+      <NavMenu isVisible={isNavMenuVisible} toggleVisible={toggleNavMenu} />
       {children}
     </>
   );
