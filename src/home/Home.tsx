9899d51aee675a28ca7368912cf14f28b03fbfc2
billy-yuan <billy.yuan1@gmail.com>
2021-11-25 22:56:13 -0500

    clean up home component

--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -1,12 +1,10 @@
 import { HomeContextProvider } from "./hooks/context";
-import { HighlightImage } from "./HighlightImage";
 import { HighlightNavSection } from "./HighlightNavSection";
 
 export function Home() {
   return (
     <HomeContextProvider>
       <div className="home-container">
-        {/* <HighlightImage /> */}
         <HighlightNavSection />
       </div>
     </HomeContextProvider>
