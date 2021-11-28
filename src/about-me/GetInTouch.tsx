da40b29c640f6cd586ebdb5ba8af6a389007dba2
billy-yuan <billy.yuan1@gmail.com>
2021-11-28 10:17:10 -0500

    update about me formatting

---
+++ src/about-me/GetInTouch.tsx
@@ -0,0 +1,16 @@
+export function GetInTouch() {
+  return (
+    <div style={{ textAlign: "left" }}>
+      <div>hello@billyyuan.com </div>
+      <div>
+        <a
+          href="http://www.linkedin.com/in/billyyuan"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          LinkedIn
+        </a>{" "}
+      </div>
+    </div>
+  );
+}
