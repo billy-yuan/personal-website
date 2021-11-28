28e9df8963a13b263cb9d8296d78329488081d35
billy-yuan <billy.yuan1@gmail.com>
2021-11-28 10:17:10 -0500

    update about me formatting

--- src/about-me/AboutMe.tsx
+++ src/about-me/AboutMe.tsx
@@ -1,3 +1,29 @@
+import { useQuery } from "@apollo/client";
+import ReactMarkdown from "react-markdown";
+import { COLORS } from "../utility/colors";
+import { GetInTouch } from "./GetInTouch";
+import { ABOUT_ME_QUERY } from "./queries";
+import "./style.css";
+
 export function AboutMe() {
-  return <>About Me</>;
+  const { data } = useQuery(ABOUT_ME_QUERY);
+
+  if (!data) {
+    return <div />;
+  }
+
+  return (
+    <div className="about-me-container">
+      <div className="left-about-me">
+        <ReactMarkdown>{data.aboutMes[0].content.markdown}</ReactMarkdown>
+      </div>
+      <div className="right-about-me">
+        <div
+          className="about-me-picture"
+          style={{ backgroundColor: COLORS.bisque }}
+        />
+        <GetInTouch />
+      </div>
+    </div>
+  );
 }
