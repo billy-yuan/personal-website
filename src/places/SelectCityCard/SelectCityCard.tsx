8e76b72d7e30ffb769a5a489d673607c019924ac
billy-yuan <billy.yuan1@gmail.com>
2021-11-29 13:22:56 -0500

    change city img to background image

--- src/places/SelectCityCard/SelectCityCard.tsx
+++ src/places/SelectCityCard/SelectCityCard.tsx
@@ -1,17 +1,17 @@
 import { COLORS } from "../../utility/colors";
+import { url } from "../../utility/utility";
 import "./style.css";
 
 type SelectCityCardProps = {
   city: string;
-  imageUrl?: string;
+  imageUrl: string;
 };
 export function SelectCityCard({ city, imageUrl }: SelectCityCardProps) {
   const baseStyle = { color: COLORS.white };
 
   return (
     <div className="select-city-card-container" style={baseStyle}>
-      {imageUrl && <img src={imageUrl} />}
-      <div>
+      <div style={{ backgroundImage: url(imageUrl) }}>
         <h2>{city}</h2>
       </div>
     </div>
