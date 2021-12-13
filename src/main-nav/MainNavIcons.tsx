9495b57cdd7d2391d3a6c79cb25d7b64a85b9400
billy-yuan <billy.yuan1@gmail.com>
2021-12-12 22:14:07 -0500

    set up i18n with react

--- src/main-nav/MainNavIcons.tsx
+++ src/main-nav/MainNavIcons.tsx
@@ -1,22 +1,44 @@
 import { useState } from "react";
+import { useTranslation } from "react-i18next";
 import { Link } from "react-router-dom";
 import { NavMenu } from "./NavMenu";
 import "./style.css";
 
 export function MainNavIcons({ children }: any) {
   const [isNavMenuVisible, toggleNavMenu] = useState<boolean>(false);
-
+  const { t, i18n } = useTranslation();
+  const selectedLanguageStyle = {
+    textDecoration: "underline",
+  };
+  console.log(i18n.language);
   return (
     <>
       <Link to="/">
         <div className="home-icon">BY</div>
       </Link>
-      <div
-        className="nav-icon"
-        style={{ color: isNavMenuVisible ? "white" : "black" }}
-        onClick={() => toggleNavMenu(!isNavMenuVisible)}
-      >
-        {isNavMenuVisible ? "x" : "="}
+      <div className="right-nav-icons">
+        <div className="choose-language">
+          <span
+            onClick={() => i18n.changeLanguage("en")}
+            style={i18n.language === "en" ? selectedLanguageStyle : {}}
+          >
+            EN
+          </span>
+          <span> / </span>
+          <span
+            onClick={() => i18n.changeLanguage("jp")}
+            style={i18n.language === "jp" ? selectedLanguageStyle : {}}
+          >
+            JP
+          </span>
+        </div>
+        <div
+          className="nav-icon"
+          style={{ color: isNavMenuVisible ? "white" : "black" }}
+          onClick={() => toggleNavMenu(!isNavMenuVisible)}
+        >
+          {isNavMenuVisible ? "x" : "="}
+        </div>
       </div>
       <NavMenu isVisible={isNavMenuVisible} toggleVisible={toggleNavMenu} />
       {children}
---
+++ src/utility/i18n/translation.ts
@@ -0,0 +1,22 @@
+export const translation = {
+  en: {
+    translation: {
+      navSection: {
+        aboutMe: "About Me",
+        blog: "Blog",
+        people: "People",
+        places: "Places",
+      },
+    },
+  },
+  jp: {
+    translation: {
+      navSection: {
+        aboutMe: "僕",
+        blog: "ブログ",
+        people: "人",
+        places: "所",
+      },
+    },
+  },
+};
