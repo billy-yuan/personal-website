3bfeadc5990cac3634764b995666ec9df2bc4d90
billy-yuan <billy.yuan1@gmail.com>
2021-12-31 22:18:06 -0500

    make select place page responsive

--- src/places/SelectCityCard/SelectCityCard.tsx
+++ src/places/SelectCityCard/SelectCityCard.tsx
@@ -1,4 +1,3 @@
-import { COLORS } from "../../utility/colors";
 import { url } from "../../utility/utility";
 import "./style.css";
 
@@ -7,13 +6,10 @@ type SelectCityCardProps = {
   imageUrl: string;
 };
 export function SelectCityCard({ city, imageUrl }: SelectCityCardProps) {
-  const baseStyle = { color: COLORS.white };
-
   return (
-    <div className="select-city-card-container" style={baseStyle}>
-      <div style={{ backgroundImage: url(imageUrl) }}>
-        <h2>{city}</h2>
-      </div>
+    <div className="select-city-card-container">
+      <div style={{ backgroundImage: url(imageUrl) }}></div>
+      <p>{city}</p>
     </div>
   );
 }
