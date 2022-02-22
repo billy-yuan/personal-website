3859f6b7da8ef5b25a56d5b8846ae89f363850f7
billy-yuan <billy.yuan1@gmail.com>
2022-02-22 11:38:39 -0500

    add graphql for projects

--- src/projects/Projects.tsx
+++ src/projects/Projects.tsx
@@ -1,7 +1,14 @@
+import { useQuery } from "@apollo/client";
 import { t } from "i18next";
+import i18n from "../i18n";
+import { PROJECTS_QUERY } from "./query";
 import "./style.css";
 
 export function Projects() {
+  const { data, loading, error, refetch } = useQuery(PROJECTS_QUERY, {
+    variables: { locale: i18n.language },
+  });
+
   return (
     <div className="projects-container">
       <div className="projects-home-header">
